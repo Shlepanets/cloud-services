30882d9f3b7f51b93c78039335b58aa9e2d4703d
Mikhail <shlepanets@gmail.com>
2019-09-05 09:29:51 +0300

    GoogleCloud init

---
+++ GoogleCloud/example.tf
@@ -0,0 +1,31 @@
+# Google Cloud
+
+provider "google" {
+  credentials = file("/home/mike/credentials/example-251920-d6feefc612ca.json")
+
+  project = "example-251920"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags	       = ["web", "test"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
