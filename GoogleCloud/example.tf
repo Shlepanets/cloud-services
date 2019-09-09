cfa7a0a1dbbfcd4f47b0514fa5f8c9b84b161480
Mikhail <shlepanets@gmail.com>
2019-09-09 23:40:00 +0300

    test multiple roles and instances

--- GoogleCloud/example.tf
+++ GoogleCloud/example.tf
@@ -1,5 +1,18 @@
 # Google Cloud
 
+variable "roles" {
+  type	= list(string)
+  description = "roles to be executed on target hosts"
+  default = [ "test", "test2" ]
+}
+
+variable "instances" {
+  type	= list(string)
+  description = "VM id's"
+  default = [ "1", "2" ]
+}
+
+
 provider "google" {
   credentials = file("/home/mike/credentials/example-251920-d6feefc612ca.json")
 
@@ -23,18 +36,21 @@ resource "google_compute_firewall" "vpc_network" {
 
 }
 
-resource "google_compute_address" "vm_static_ip" {
-  name = "terraform-static-ip"
-}
+#resource "google_compute_address" "vm_static_ip" {
+#  count = 3
+#  name = "terraform-static-ip-${count.index}"
+#}
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
+  count = "${length(var.roles) * length(var.instances)}"
+
+  name         = "instance-${var.roles[floor(count.index / length(var.instances))]}-${var.instances[count.index % length(var.instances)]}"
   machine_type = "f1-micro"
   tags	       = ["web", "test"]
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "centos-7-v20190813"
     }
   }
   
@@ -45,18 +61,18 @@ resource "google_compute_instance" "vm_instance" {
   network_interface {
     network = google_compute_network.vpc_network.self_link
     access_config {
-      nat_ip = google_compute_address.vm_static_ip.address
+#      nat_ip = google_compute_address.vm_static_ip[count.index].address
     }
   }
 
   provisioner "local-exec" {
-    command = "echo ${google_compute_instance.vm_instance.name}:  ${google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip} >> ip_address.txt"
+    command = "echo ${self.name}: ${self.network_interface[0].access_config[0].nat_ip}  >> ip-tables.txt"
   }
-
-  provisioner "local-exec" {
-    command = "echo ${google_compute_instance.vm_instance.name}:  ${google_compute_address.vm_static_ip.address} >> ip_address.txt"
-  }
-
+#
+#  provisioner "local-exec" {
+#    command = "echo ${google_compute_instance.vm_instance.name}:  ${google_compute_address.vm_static_ip.address}"
+#  }
+#
   provisioner "remote-exec" {
     inline = ["sudo touch in-test.ll"]
 
@@ -64,12 +80,12 @@ resource "google_compute_instance" "vm_instance" {
       type        = "ssh"
       user        = "shlepanets"
       private_key = "${file(var.ssh_key_private)}"
-      host        = "${google_compute_address.vm_static_ip.address}"
+      host        = "${self.network_interface[0].access_config[0].nat_ip}"
     }
   }
 
   provisioner "local-exec" {
-    command = "ansible-playbook -u shlepanets -i '${google_compute_address.vm_static_ip.address},' --private-key ${var.ssh_key_private} main.yml" 
+    command = "ansible-playbook -u shlepanets -i '${self.network_interface[0].access_config[0].nat_ip},' --private-key ${var.ssh_key_private} --tags ${var.roles[floor(count.index / length(var.instances))]} main.yml" 
   }
 
 }
